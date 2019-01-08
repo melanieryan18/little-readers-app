ae90259037478a42bbb7c6ba46b0458daa29901a
Melanie Ryan <melaniehoperyan@gmail.com>
2019-01-08 13:49:54 -0800

    first commit

---
+++ models/example.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+  var Example = sequelize.define("Example", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+
+  Example.associate = function(models) {
+    models.Example.belongsTo(models.User, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return Example;
+};
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,41 @@
+var db = require("../models");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+module.exports = function(app) {
+  // Load signup page
+  app.get("/", function(req, res) {
+    return res.render("signup");
+  });
+
+  // Load login page
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+
+  // Load profile page
+  app.get("/profile", isAuthenticated, function(req, res) {
+    db.User.findOne({
+      where: {
+        id: req.user.id
+      },
+      include: [db.Example]
+    }).then(function(dbUser) {
+      res.render("profile", { user: dbUser });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", isAuthenticated, function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
