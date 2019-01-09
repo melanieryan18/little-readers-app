f71dee5aff3dfc6d0ad41497e8590f80bc4b7fa3
JaniceNova <janicenovacek2016@gmail.com>
2019-01-09 10:47:57 -0800

    added js Bookshelve

---
+++ public/js/bookshelf.js
@@ -0,0 +1,17 @@
+
+//makes new book on add book
+$(document).ready(function(){
+    $(".addBook").click(function(){
+      $(".nextBook").append("<div class='a'><img class='haveRead' src='images/greenBook.png'><input type='text' class='inputBook' ><div class='addTitle' >Add Title</div></div>");
+      
+    });
+  });
+
+
+  //grabs input from input field
+$("body").on("click", ".addTitle", function(){
+  console.log($(this).parent().children()[1].value)
+ var BookInput= $(this).parent().children()[1].value
+
+  $(this).parent().html("<div class='a'><img class='haveRead' src='images/greenBook.png'><div class=BooksIHaveRead>" + BookInput + "</div><div class='deleteBook' >DeleteBook</div></div>");
+})
