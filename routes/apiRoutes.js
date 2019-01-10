5be8ae0849c4bbd27252976f19231b38a57959cb
Melanie Ryan <melaniehoperyan@gmail.com>
2019-01-10 10:47:04 -0800

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,46 +3,10 @@ var passport = require("../config/passport");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", isAuthenticated, function(req, res) {
-    db.Example.findAll({
-      where: {
-        UserId: req.user.id
-      }
-    }).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", isAuthenticated, function(req, res) {
-    db.Example.create({
-      UserId: req.user.id,
-      text: req.body.text,
-      description: req.body.description
-    }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", isAuthenticated, function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
-
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will happen on the front end
-    // They won't get this or even be able to access this page if they aren't authed
-    res.json(req.user);
+    res.render(profile);
   });
+  // ROUTE HERE TO DASHBOARD BEFORE - ONLY ONE RES.RENDER(DASHBOARD PAGE)
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
@@ -54,14 +18,34 @@ module.exports = function(app) {
       password: req.body.password,
       firstName: req.body.firstName,
       lastName: req.body.lastName
+    }).then(function() {
+      res.redirect(307, "/api/login");
+    });
+  });
+
+  app.post("api/games", function(req, res) {
+    console.log(req.body);
+    db.Book.create({
+      wins: req.body.wins,
+      losses: req.body.losses,
+      difficulty: req.body.difficulty
     })
-      .then(function() {
-        res.redirect(307, "/api/login");
-      })
       .catch(function(err) {
         res.status(422).json(err.errors[0].message);
       });
   });
+
+  app
+    .post("api/profile/book", function(req, res) {
+      console.log(req.body);
+      db.Book.create({
+        bookName: req.body.bookName,
+        read: req.body
+      });
+    })
+    .catch(function(err) {
+      res.status(422).json(err.errors[0].message);
+    });
 
   // Route for logging user out
   app.get("/logout", function(req, res) {
